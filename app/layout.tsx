9e34d1523a2498d3035eca474145ad8b12af27b1
Ismail Hassan Yusuf <mugathmanmotors.dev@gmail.com>
2025-11-16 09:58:02 +0100

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 import Navigator from "@/components/Navigators";
-import GoogleAnalytics from "@/components/GoogleAnalytics";
+import { GoogleAnalytics } from "@next/third-parties/google"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -53,8 +53,8 @@ export default function RootLayout({ children }) {
           <Navigator responsive={true} />
         </header>
         {children}
-        <GoogleAnalytics />
       </body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
     </html>
   );
 }
